05912c5998b24f8a23e46e63fbd1e98089d068e6
farrelraditya <farrelradityasecond2@gmail.com>
2025-05-25 13:35:22 +0700

    Initial commit

---
+++ app/src/main/java/edu/uph/m23si1/farrelapp/FarrelActivity.kt
@@ -0,0 +1,20 @@
+package edu.uph.m23si1.farrelapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FarrelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
